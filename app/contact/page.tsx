4b72d2935769fe72c2185d71f810c0609a7e6a17
Hardik Gohel <32968507+hagohel@users.noreply.github.com>
2026-02-15 18:53:02 -0600

    Fix import statements in ContactPage component

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
-import { Section } from '../../components/section'
-import { Button } from '../../components/ui/button'
+import { Section } from "../../components/section";
+import { Button } from "../../components/ui/button";
 import { Card, CardContent, CardHeader } from "../../components/ui/card";
 
 export default function ContactPage() {
