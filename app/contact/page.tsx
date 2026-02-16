13b06bd1f86d246211a959bf72f96402e226235c
Hardik Gohel <32968507+hagohel@users.noreply.github.com>
2026-02-15 18:41:40 -0600

    Import useState from React in contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import { useState } from "react";
 import { Section } from '../../components/section'
 import { Button } from '../../components/ui/button'
 import { Card } from '../../components/ui/card'
